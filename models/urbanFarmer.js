be63f3145e598680f29262ab0651fd7e8b0eaa08
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2020-11-10 21:32:41 +0300

    routing functionality

---
+++ models/urbanFarmer.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+// Create a schema for farmer one.
+const urbanFarmerSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Array,
+    required: true,
+  },
+  nin: {
+    type: String,
+    required: true,
+  },
+  ward: {
+    type: String,
+    required: true,
+  },
+  horticulture: {
+    type: String,
+    default: "off",
+  },
+  poultry: {
+    type: String,
+    default: "off",
+  },
+  diary: {
+    type: String,
+    default: "off",
+  },
+  ID: {
+    type: String,
+    required: true,
+  },
+  registrationDate: {
+    type: String,
+    required: true,
+  },
+});
+
+// Export a model to create documents.
+module.exports = mongoose.model("urbanfarmer", urbanFarmerSchema);
--- routes/foRoutes.js
+++ routes/foRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const UrbanFarmer = require("../models/urbanFarmer");
 
 // Routes.
 router.get("/", (req, res) => {
@@ -10,6 +11,29 @@ router.get("/register", (req, res) => {
   res.render("register_urban");
 });
 
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+  try {
+    // Construct document object.
+    const urbanFarmer = new UrbanFarmer({
+      name: req.body.name,
+      dob: req.body.dob,
+      gender: req.body.gender,
+      phoneNumber: req.body.phoneNumber,
+      nin: req.body.nin,
+      ward: req.body.ward,
+      horticulture: req.body.horticulture,
+      poultry: req.body.poultry,
+      diary: req.body.diary,
+      ID: req.body.ID,
+      registrationDate: req.body.registrationDate,
+    });
+
+    await urbanFarmer.save(() => {
+      res.redirect("/fo");
+    });
+  } catch (err) {
+    console.log({ message: err });
+  }
+});
 
 module.exports = router;
