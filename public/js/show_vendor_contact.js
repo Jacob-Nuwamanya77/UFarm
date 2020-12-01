9be9c532c042499f55dd1a2d2cf8f998c8ec7e3d
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2020-12-01 08:17:50 +0300

    Working on the selection of categories

--- public/js/show_vendor_contact.js
+++ public/js/show_vendor_contact.js
@@ -111,4 +111,25 @@
   [].forEach.call(vendorPhones, (phone) => {
     phone.addEventListener("click", showVendor);
   });
+
+  // Access the button to show categories available.
+  let dropBtn = document.querySelector(".dropbtn");
+  dropBtn.addEventListener("click", open_menu);
+  function open_menu() {
+    document.querySelector("#product-list").classList.toggle("show");
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function (event) {
+    if (!event.target.matches(".dropbtn")) {
+      var dropdowns = document.querySelectorAll(".dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  };
 })();
